585f2273145e96026d54c2b76c33ee1756f803f7
enzofloresb <enzoflores2@gmail.com>
2020-08-18 18:06:22 -0300

    homeworks 1, 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -83,6 +83,7 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 
   if (numero < 2) return false;
+  if (numero === 2) return true;
 
   for(var i = 2; i < numero; i++){
     if(numero % i === 0) {
